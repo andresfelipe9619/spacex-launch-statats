3eac8c988c7c926fd6c467cc00b506fe137345ee
Andrés Suárez <andresfelipe9619@gmail.com>
2019-04-19 10:14:03 -0500

    Add routing and styles

--- client/src/components/launches.js
+++ client/src/components/launches.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import LaunchItem from "./launch-item";
+import MissionKey from "./mission-key";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -13,15 +14,18 @@ const LAUNCHES_QUERY = gql`
     }
   }
 `;
+
 export class Launches extends Component {
   render() {
     return (
       <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
+        <MissionKey />
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
-            if (loading) return <h4>Loading ...</h4>;
-            if (error) console.log("Error=>", error);
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
+
             return (
               <Fragment>
                 {data.launches.map(launch => (
